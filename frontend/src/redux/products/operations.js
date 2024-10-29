70eacb710483057a084ee7901b00507dcfa4e82f
JulkaUlka <y.glushko@outlook.com>
2024-10-29 23:53:24 +0200

    chore: update backend URL for frontend requests

--- frontend/src/redux/products/operations.js
+++ frontend/src/redux/products/operations.js
@@ -5,7 +5,9 @@ export const getProducts = createAsyncThunk(
   "products/getProducts",
   async (thunkAPI) => {
     try {
-        const result = await axios.get("/api/products");
+      const result = await axios.get(
+        "https://organick-c7ws.onrender.com/api/products"
+      );
       return result.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
